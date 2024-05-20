01fa9f876e092190fe7d33892713fa209e10836d
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-21 05:24:23 +0530

    Time: 59 ms (35.85%), Space: 12 MB (26.42%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def shortestCompletingWord(self, licensePlate, words):
+        """
+        :type licensePlate: str
+        :type words: List[str]
+        :rtype: str
+        """
+        collector=[]
+        sorLP=sorted([val for val in licensePlate.lower() if val.isalpha()])
+        for j in words:
+            j=j.lower()
+            if all([sorLP.count(i)<=j.count(i) for i in sorLP]):
+                collector.append(j)
+        sorted_collector=sorted(collector,key=lambda x: len(x))
+        return sorted_collector[0]
+        
