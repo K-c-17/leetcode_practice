79bb07b958083f3ada657970b7860432f6b43f49
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-21 03:29:41 +0530

    Time: 1265 ms (5.65%), Space: 15.4 MB (89.65%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        collector={}
+        for i in strs:
+            sorted_str=''.join(sorted(i))
+            if sorted_str not in collector.keys():
+                collector[sorted_str]=[i]
+            else:
+                collector[sorted_str].append(i)
+        return [val for val in collector.values()]
+            
+            
+            
+        
