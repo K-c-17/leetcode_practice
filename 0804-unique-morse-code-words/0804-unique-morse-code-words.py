e4079bc8b60e515f63e71fda4456e341abc4116b
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-23 01:48:41 +0530

    Time: 24 ms (16.80%), Space: 11.8 MB (34.99%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.py
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -5,16 +5,13 @@ class Solution(object):
         :rtype: int
         """
         code_list=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
-        alpha_list=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-                    
-        alpha_code_map={alpha_list[i]:code_list[i] for i in range(len(alpha_list))}
+        transformed_code={"".join(code_list[ord(i)-ord('a')] for i in val) for val in words}
         
-        start=''
-        collector=[]
-        for val in words:
-            for alp in val:
-                start+=alpha_code_map[alp]
-            if start not in collector:
-                collector.append(start)
-            start=''
-        return len(collector)
+        return len(transformed_code)
+                    
+        
+
+
+
+
+
