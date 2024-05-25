9dcc22161f9f63e9031bc10f3fd8ae14431cbd29
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-25 18:26:00 +0530

    Time: 2127 ms (5.05%), Space: 12.7 MB (94.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,8 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        """
         for i in range(0,len(nums)+1):
             if i not in nums:
                 return i
-        #return [i for i in range(0,len(nums)+1) if i not in nums][0] 
+        """
+        return [i for i in range(0,len(nums)+1) if i not in nums][0] 
         
