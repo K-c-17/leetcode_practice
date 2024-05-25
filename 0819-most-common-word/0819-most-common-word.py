b70d4b93fdad4853f1ba5780c4942790e9bbdc7b
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-25 18:12:15 +0530

    Time: 31 ms (10.03%), Space: 11.7 MB (87.32%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -8,15 +8,12 @@ class Solution(object):
         collector={}
         new_para=''
         #pre-processing of the input paragraph
-        for val in paragraph:
-            if val in "!?',;.":
-                new_para+=' '
-            else:
-                new_para+=lower(val)
-        para_lower=[i for i in new_para.split(' ') if len(i)<>0 and i not in banned]
+        new_para=''.join([lower(val) if val.isalnum() else ' ' for val in paragraph])
+        para_lower=[i for i in new_para.split(' ') if len(i)<>0 ]
         print(para_lower)
         for i in set(para_lower):
-                collector[i]=para_lower.count(i)
+                if i not in banned:
+                    collector[i]=para_lower.count(i)
         #return max(collector.keys(),key=lambda x: collector[x])
         return max(collector.keys(),key=collector.get)
             
