29348a831da67049a6f8a00adcf64c7d2b4c55b2
K-c-17 <57230059+K-c-17@users.noreply.github.com>
2024-05-25 17:54:01 +0530

    Time: 25 ms (25.37%), Space: 11.7 MB (87.32%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -13,13 +13,10 @@ class Solution(object):
                 new_para+=' '
             else:
                 new_para+=lower(val)
-        para_lower=[i for i in new_para.split(' ') if len(i)<>0]
+        para_lower=[i for i in new_para.split(' ') if len(i)<>0 and i not in banned]
         print(para_lower)
         for i in set(para_lower):
-            if i not in banned:
                 collector[i]=para_lower.count(i)
-            else:
-                pass
         #return max(collector.keys(),key=lambda x: collector[x])
         return max(collector.keys(),key=collector.get)
             
