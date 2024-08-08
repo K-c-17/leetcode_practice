b1458a57168ce6c8c387736556fc7a4864ed8c0e
kshitij <57230059+K-c-17@users.noreply.github.com>
2024-08-08 18:46:39 -0400

    Time: 26 ms (76.63%), Space: 11.8 MB (17.73%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -5,22 +5,26 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        combined=[nums1,nums2]
-        collector=[]
-        for lst in combined:
-            def_dict=dict.fromkeys(range(0,1001),0)
-            for num in lst:
-                def_dict[num]+=1
-            collector.append(def_dict)
+        dict1={}
+        dict2={}
+        for i in nums1:
+            if i in dict1:
+                dict1[i]+=1
+            else:
+                dict1[i]=1
+        for j in nums2:
+            if j in dict2:
+                dict2[j]+=1
+            else:
+                dict2[j]=1
         
         min_dict={}
-        for val in collector:
-            for i in val:
-                if i not in min_dict or min_dict[i]>val[i]:
-                    min_dict[i]=val[i]
-                else:
-                    pass
-        
+        for key,value in dict1.items():
+            if key in dict2:
+                min_dict[key]=min(dict1[key],dict2[key])
+            else:
+                pass
+
         return [key for key,value in min_dict.items() for _ in range(value)]
 
 
