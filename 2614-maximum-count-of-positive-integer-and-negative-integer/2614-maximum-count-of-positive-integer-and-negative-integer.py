a3edd75d1db25bbcbd335f04a698e218baaadd6b
kshitij <57230059+K-c-17@users.noreply.github.com>
2024-10-13 02:24:40 -0400

    Time: 88 ms (78.67%), Space: 11.7 MB (91.64%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,17 +1,41 @@
 class Solution(object):
+
     def maximumCount(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        #initializing the counters
-        pos=0
-        neg=0
-        #traversal
-        for i in nums:
-            if i > 0:
-                pos+=1
-            elif i < 0:
-                neg+=1
+        if nums[-1] < 0 or nums[0] > 0:
+            return len(nums)
+        if nums[0] == 0 and nums[-1] == 0:
+            return 0
+
+        negative_count = 0
+        positive_count = 0
+        left, right = 0, len(nums) - 1
+
+        # Find positive count
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > 0:
+                right = mid - 1
+                positive_count = mid
+            else:
+                left = mid + 1
+
+        if positive_count != 0:
+            positive_count = len(nums) - positive_count
+
+        left, right = 0, len(nums) - 1
+
+        # Find negative count
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < 0:
+                left = mid + 1
+                negative_count = mid
+            else:
+                right = mid - 1
+
+        if negative_count != 0:
+            negative_count += 1
+
+        return max(positive_count, negative_count)
         
-        return max(pos,neg)
+
