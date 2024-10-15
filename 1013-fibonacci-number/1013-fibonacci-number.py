666020f442a3d564166abe27cb2a9863cdfffc2e
kshitij <57230059+K-c-17@users.noreply.github.com>
2024-10-14 22:10:29 -0400

    Time: 1105 ms (5.11%), Space: 11.6 MB (29.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        assert n>=0 and int(n)==n,"The input can only be non-negative integer"
         if n in [0,1]:
             return n
         #final function return
