658ae4cff0af44d0dfc8e223ee206b44cbe2353d
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-04 17:55:06 -0500

    Time: 0 ms (100%), Space: 12.5 MB (20.1%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,19 +5,6 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # is_match=0
-        # match_index=float('-inf')
-        # for i in range(len(s)):
-        #     for j in range(len(t)):
-        #         if s[i]==t[j] and j>match_index:
-        #             match_index=j
-        #             is_match+=1
-        #             break
-        # if is_match==len(s):
-        #     return True
-        # else:
-        #     return False
-        
         is_match=0
         i=0
         j=0
