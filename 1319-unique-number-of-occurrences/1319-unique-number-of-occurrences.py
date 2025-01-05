f27cbe4a377ef3825e1e12994920be122c5aabdb
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-05 16:36:08 -0500

    Time: 31 ms (5.1%), Space: 12.5 MB (15.5%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -4,14 +4,7 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        collector={}
-
-        for i in arr:
-            if i not in collector.keys():
-                collector[i]=1
-            else:
-                collector[i]+=1
-        
+        collector={i:arr.count(i) for i in arr}
         unique_frequency=[]
 
         for j in collector.values():
