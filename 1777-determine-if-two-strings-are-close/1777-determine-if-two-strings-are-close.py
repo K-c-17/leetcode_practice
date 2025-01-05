431fdc0536f2b671213386824a1c75ee9b3f907f
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-05 17:28:02 -0500

    Time: 185 ms (72%), Space: 13.1 MB (37.62%) - LeetHub

---
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def closeStrings(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        freq1={}
+        freq2={}
+        for i in word1:
+            if i not in freq1:
+                freq1[i]=1
+            else:
+                freq1[i]+=1
+        
+        for j in word2:
+            if j not in freq2:
+                freq2[j]=1
+            else:
+                freq2[j]+=1
+        
+        sort_freq1=sorted([x for x in freq1.values()])
+        sort_freq2=sorted([y for y in freq2.values()])
+
+        if {x for x in freq1.keys()}.difference({y for y in freq2.keys()})!=set():
+            print('1')
+            print({x for x in freq1.keys()})
+            print({y for y in freq2.keys()})
+            print({x for x in freq1.keys()}.difference({y for y in freq2.keys()}))
+            return False
+        elif sort_freq1!=sort_freq2:
+            print('2')
+            return False
+        else:
+            print('3')
+            return True
+
+        
