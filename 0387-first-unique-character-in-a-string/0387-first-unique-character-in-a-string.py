c73dc5a709a45065f54c759a24dc922930d281d8
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-09 20:33:37 -0500

    Time: 172 ms (15.4%), Space: 16 MB (6.48%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -4,16 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        collector={}
-        #finding the frequency of every alphabet and storing this in a hash map
-        for alphabet in s:
-            if alphabet not in collector.keys():
-                collector[alphabet]=1
-            else:
-                collector[alphabet]+=1
-        
-        #going over my input string and finding the first alphabet which is having a frequency of 1
+        freq=Counter(s)
+
         for i in range(len(s)):
-            if collector[s[i]]==1:
+            if freq[s[i]]==1:
                 return i
         return -1
