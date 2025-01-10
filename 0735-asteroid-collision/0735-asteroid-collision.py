a5da2f120564e543c3df8756c93fa416d7a2e88d
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-10 18:34:41 -0500

    Time: 7 ms (84.72%), Space: 13.3 MB (25.32%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type asteroids: List[int]
         :rtype: List[int]
         """
-        stage=[]
+        stack=[]
         for i in asteroids:
-            while len(stage)<>0 and i<0 and stage[-1]>0:
-                top=stage.pop()
-                if top>abs(i):
-                    stage.append(top)
-                    break
-                elif top==abs(i):
-                    break
+            while stack and stack[-1]>0 and i<0:
+                diff=stack[-1]-abs(i)
+                if diff>0:
+                    i=0
+                elif diff<0:
+                    stack.pop()
                 else:
-                    continue
-            else:
-                stage.append(i)
-        return stage
+                    stack.pop()
+                    i=0
+            if i:
+                stack.append(i)
+        return stack
+        
 
 
         
