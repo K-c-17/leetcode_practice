96cf1ae64ccb54b36b0a64a53475201e9d374020
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-14 06:48:51 -0500

    Time: 95 ms (79.75%), Space: 12.9 MB (91.3%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -7,19 +7,14 @@ class Solution(object):
         """
         start,end=0,k
         vowels={'a', 'e', 'i', 'o', 'u'}
-        print('doubtful statement')
+        
         comp_size=sum(1 for char in s[start:end] if char in vowels)
         max_size=comp_size
-        print('loop starts')
         while end<=len(s)-1:
             
-            #print('string under considertion',s[start:end])
-            
             if s[start] in vowels and s[end] not in vowels:
-                print('first')
                 comp_size-=1
             elif s[start] not in vowels and s[end] in vowels:
-                print('second')
                 comp_size+=1
             
             if comp_size>max_size:
@@ -27,6 +22,5 @@ class Solution(object):
             
             start+=1
             end+=1
-            #print('comp_size: ',comp_size)
-            #print('max_size:',max_size)
+
         return max_size
