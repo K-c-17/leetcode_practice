fa91b9480e9515e870e744c4b94b5f5e49338df7
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-01-18 19:34:34 -0500

    Time: 4689 ms (14.26%), Space: 18.6 MB (27.21%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -12,21 +12,20 @@ class Solution(object):
             #print('number of iteration: ',i)
             if i>0 and sor_nums[i]==sor_nums[i-1]:
                 continue
-            else:
-                residual_sum=target-sor_nums[i]
-                left=i+1
-                right=len(nums)-1
-                while right>left:
-                    comp=sor_nums[left]+sor_nums[right]
-                    if comp>residual_sum:
-                        right-=1
-                    elif comp<residual_sum:
-                        left+=1
-                    else:
-                        if [sor_nums[i],sor_nums[left],sor_nums[right]] not in res:
-                            res.append([sor_nums[i],sor_nums[left],sor_nums[right]])
-                        right-=1
-                    #print('value of res:',res)
+            residual_sum=target-sor_nums[i]
+            left=i+1
+            right=len(nums)-1
+            while right>left:
+                comp=sor_nums[left]+sor_nums[right]
+                if comp>residual_sum:
+                    right-=1
+                elif comp<residual_sum:
+                    left+=1
+                else:
+                    if [sor_nums[i],sor_nums[left],sor_nums[right]] not in res:
+                        res.append([sor_nums[i],sor_nums[left],sor_nums[right]])
+                    right-=1
+                #print('value of res:',res)
         return res
 
 
