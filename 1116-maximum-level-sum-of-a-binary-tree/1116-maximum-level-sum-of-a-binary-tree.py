2817d4412cde4f8258df7026a93da854983fb73e
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-02-08 04:57:46 -0500

    Time: 51 ms (61.47%), Space: 20.6 MB (78.74%) - LeetHub

--- 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/1116-maximum-level-sum-of-a-binary-tree.py
@@ -11,21 +11,23 @@ class Solution(object):
         :rtype: int
         """
         queue = collections.deque()
-        queue.append((root,1))
-        final=defaultdict()
+        queue.append(root)
+        sum_at_level,level=float('-inf'),0
         
         while queue:
             temp=0
-            for i in range(len(queue)):
-                current,level = queue.popleft()
+            level+=1
+            for _ in range(len(queue)):
+                current = queue.popleft()
                 temp+=current.val
                 if current.left:
-                    queue.append((current.left,level+1))
+                    queue.append(current.left)
                 if current.right:
-                    queue.append((current.right,level+1))
-            final[level]=temp
-        
-        return max(final,key=final.get)
+                    queue.append(current.right)
+            if temp>sum_at_level:
+                sum_at_level,min_level=temp,level
+       
+        return min_level
 
                 
                 
