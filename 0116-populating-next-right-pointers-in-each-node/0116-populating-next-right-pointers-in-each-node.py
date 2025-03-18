1cf12e5b5115bdbf8b419e8a1900c7d8f03e8974
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-18 04:14:15 -0400

    Time: 42 ms (60.06%), Space: 15.5 MB (58.31%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -16,55 +16,6 @@ class Solution(object):
         """
         if not root:
             return root
-        
-        queue=collections.deque()
-        queue.append(root)
-
-        while queue:
-            val=len(queue)
-            for i in range(val):
-                current=queue.popleft()
-                if i!=val-1:
-                    current.next=queue[0]
-                if current.left:
-                    queue.append(current.left)
-                if current.right:
-                    queue.append(current.right)
-        return root
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        '''
-        if not root:
-            return root
         queue = collections.deque()
         queue.append(root)
         
@@ -80,5 +31,4 @@ class Solution(object):
                 if current.right:
                     queue.append(current.right)
         return root
-        '''
         
