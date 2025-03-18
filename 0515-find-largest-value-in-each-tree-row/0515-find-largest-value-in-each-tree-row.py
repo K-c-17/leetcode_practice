0df5dad302dfbf6754ca1c036e6f2ad18e1058e5
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-18 16:22:49 -0400

    Time: 3 ms (79.9%), Space: 17 MB (48.04%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -14,47 +14,6 @@ class Solution(object):
             return []
         queue=collections.deque()
         queue.append(root)
-        output=[]
-
-        while queue:
-            max_val=float("-inf")
-            for _ in range(len(queue)):
-                current=queue.popleft()
-                if current.val>max_val:
-                    max_val=current.val
-                if current.left:
-                    queue.append(current.left)
-                if current.right:
-                    queue.append(current.right)
-            
-            output.append(max_val)
-        return output
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        '''
-        if not root:
-            return []
-        queue=collections.deque()
-        queue.append(root)
         result=[]
 
         while queue:
@@ -69,4 +28,3 @@ class Solution(object):
                     queue.append(current.right)
             result.append(var)
         return result
-        '''
