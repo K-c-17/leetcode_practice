fec9467c7883e79a07790cd18105aad7375604cb
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-19 02:56:55 -0400

    Time: 123 ms (81.1%), Space: 66.8 MB (55.25%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -11,11 +11,45 @@ class Solution(object):
         """
         #dummy=ListNode()
         #tail=dummy
+        '''
         output=[]
 
         while head:
             output.append(head.val)
             head=head.next
         return output==output[::-1]
+        '''
+        fast=head
+        slow=head
+
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        
+        prev_node=None
+        
+        while slow:
+            temp=slow.next
+            slow.next=prev_node
+            prev_node=slow
+            slow=temp
+        
+        start=head
+        end=prev_node
+        
+        #print("Start of the loop:", start.val)
+        #print("End of the loop:", end.val)
+
+        while end:
+            #print("value of start:",start.val,start.next)
+            #print("value of end:",end.val,end.next)
+            if start.val!=end.val:
+                return False
+            start=start.next
+            end=end.next
+        
+        return True
+
+
         
         
