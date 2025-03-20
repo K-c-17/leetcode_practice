5fbcc321aae354d6aa7619872e9ec1bdaf53e78a
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-20 19:33:36 -0400

    Time: 6 ms (76.58%), Space: 12.5 MB (84.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,33 +10,6 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        def show(lst):
-            output_str=""
-            while lst:
-                output_str+=str(lst.val)+"->"
-                lst=lst.next
-            print(output_str)
-        
-        def reverse(lst):
-            current=lst
-            prev_node=None
-
-            while current:
-                temp=current.next
-                current.next=prev_node
-                prev_node=current
-                current=temp
-            return prev_node
-        
-        #rev_l1=reverse(l1)
-        #rev_l2=reverse(l2)
-
-        #show(rev_l1)
-        #show(rev_l2)
-        show(l1)
-        show(l2)
-
-        #index,curr1,curr2,sentinel,carry=0,rev_l1,rev_l2,ListNode(),0
         index,curr1,curr2,sentinel,carry=0,l1,l2,ListNode(),0
 
         start=sentinel
