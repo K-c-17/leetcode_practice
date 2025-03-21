0774cfaf3bca86a8eca9fa6118b79bf446f89b5d
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-21 18:58:58 -0400

    Time: 2 ms (69.41%), Space: 13.2 MB (87.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,6 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        l=0
+        r=1
+
+        while r<len(nums):
+            if nums[l]==nums[r]:
+                r+=1
+            else:
+                nums[l+1],nums[r]=nums[r],nums[l+1]
+                l+=1
+                r+=1
+        
+        return len(nums[0:l+1])
+
+        
+        
+        
+        
+        
+        
+        '''
         i=1
         while i!=len(nums):
             if nums[i]==nums[i-1]:
@@ -12,5 +32,6 @@ class Solution(object):
                 i+=1
         
         return len(nums)
+        '''
 
         
