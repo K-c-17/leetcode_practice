8dbebab79ddf383ca1148d48938f589e009022a7
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-22 18:11:07 -0400

    Time: 111 ms (5.03%), Space: 13.7 MB (39.65%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -13,7 +13,7 @@ class Solution(object):
         for i in range(len(s)):
             if s[i] in mapping.keys() and mapping[s[i]]!=t[i]:
                 return False
-            elif s[i] not in mapping.keys() and t[i] in mapping.values():
+            elif s[i] not in mapping.keys() and t[i] in set(mapping.values()):
                 return False
             else:
                 mapping[s[i]]=t[i]        
