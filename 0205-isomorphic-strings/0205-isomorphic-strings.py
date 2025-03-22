f229f15e79e17813d7cb21030f72318ae1185c25
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-22 18:10:35 -0400

    Time: 111 ms (5.03%), Space: 13.6 MB (57.47%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        mapping=defaultdict(str)
+
+        if len(s)!=len(t):
+            return False
+
+        for i in range(len(s)):
+            if s[i] in mapping.keys() and mapping[s[i]]!=t[i]:
+                return False
+            elif s[i] not in mapping.keys() and t[i] in mapping.values():
+                return False
+            else:
+                mapping[s[i]]=t[i]        
+        return True
+
+        
+        
