765706503e88040e5f5d129b62b66987e38801d5
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-24 01:50:05 -0400

    Time: 10 ms (27.25%), Space: 22.3 MB (88.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,6 +5,77 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        #O(n) time complexity
+        '''
+        idx=k-((k//len(nums))*len(nums))
+        prev=nums[0]
+        while True:
+            temp=nums[idx]
+            nums[idx]=prev
+            prev=temp
+            
+            if idx==0:
+                return nums
+            else:
+                idx=(idx+k)-((idx+k)//len(nums)*len(nums))
+        '''
+        if len(nums)==1:
+            return nums
+     
+        #reverse the entire array
+        start=0
+        last=len(nums)-1
+
+        while start<last:
+            nums[start],nums[last]=nums[last],nums[start]
+            start+=1
+            last-=1
+        
+        #reverse the first k elements
+        start=0
+        last=k%len(nums) - 1
+
+        while start<last:
+            nums[start],nums[last]=nums[last],nums[start]
+            start+=1
+            last-=1
+        
+        #reverse the last len(nums)-k elements
+        start=k%len(nums)
+        last=len(nums)-1
+
+        while start<last:
+            nums[start],nums[last]=nums[last],nums[start]
+            start+=1
+            last-=1
+        
+        return nums
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        #O(n*k) time complexity
+        '''
+        for _ in range(k):
+            temp=nums[0]
+            for i in range(1,len(nums)):
+                temp2=nums[i]
+                nums[i]=temp
+                temp=temp2
+                if i==len(nums)-1:
+                    nums[0]=temp
+
+        return nums
+        '''        
+        
+        #O(N) space complexity
+        '''
         final=[None for _ in nums]
         size=len(nums)
         for r in range(size):
@@ -16,3 +87,4 @@ class Solution(object):
         nums[:]=final
         
         return nums
+        '''
