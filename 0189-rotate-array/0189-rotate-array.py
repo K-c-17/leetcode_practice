b0cec0106124367830a9227a5e18eee7208647b5
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-24 01:51:27 -0400

    Time: 11 ms (24.36%), Space: 22.2 MB (96.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,19 +6,6 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         #O(n) time complexity
-        '''
-        idx=k-((k//len(nums))*len(nums))
-        prev=nums[0]
-        while True:
-            temp=nums[idx]
-            nums[idx]=prev
-            prev=temp
-            
-            if idx==0:
-                return nums
-            else:
-                idx=(idx+k)-((idx+k)//len(nums)*len(nums))
-        '''
         if len(nums)==1:
             return nums
      
@@ -50,41 +37,3 @@ class Solution(object):
             last-=1
         
         return nums
-
-
-
-        
-        
-        
-        
-        
-        
-        
-        #O(n*k) time complexity
-        '''
-        for _ in range(k):
-            temp=nums[0]
-            for i in range(1,len(nums)):
-                temp2=nums[i]
-                nums[i]=temp
-                temp=temp2
-                if i==len(nums)-1:
-                    nums[0]=temp
-
-        return nums
-        '''        
-        
-        #O(N) space complexity
-        '''
-        final=[None for _ in nums]
-        size=len(nums)
-        for r in range(size):
-            if r+k<size:
-                final[r+k]=nums[r]
-            else:
-                loop=(r+k)//size
-                final[r+k-(loop*size)]=nums[r]
-        nums[:]=final
-        
-        return nums
-        '''
