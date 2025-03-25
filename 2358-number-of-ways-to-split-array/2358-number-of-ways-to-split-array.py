cd5f138f578f4bd0de2e47cda37024383d15bd5f
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-25 02:51:44 -0400

    Time: 3143 ms (5.22%), Space: 28 MB (6.43%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def waysToSplitArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_sum=[0]*len(nums)
+        right_sum=[0]*len(nums)
+
+        prev=0
+
+        for i in range(len(nums)):
+            prev+=nums[i]
+            left_sum[i]=prev
+        
+        prev=0
+        for j in range(len(nums)-1,-1,-1):
+            right_sum[j]=prev
+            prev+=nums[j]
+        
+        print(left_sum)
+        print(right_sum)
+        
+        valid=0
+        for k in range(len(left_sum)-1):
+            if left_sum[k]>=right_sum[k]:
+                valid+=1
+        return valid
+
+
+        
