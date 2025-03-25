3a8583b28b490906258fb43b62d48d16dcb1333f
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-25 02:01:43 -0400

    Time: 19 ms (27.42%), Space: 14.1 MB (6.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,6 +4,46 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        left_sum=[0]*len(nums)
+        right_sum=[0]*len(nums)
+
+        prev=0
+        for i in range(len(nums)):
+            left_sum[i]=prev
+            prev+=nums[i]
+        
+        prev=0
+        for j in range(len(nums)-1,-1,-1):
+            right_sum[j]=prev
+            prev+=nums[j]
+        
+        # print(left_sum)
+        # print(right_sum)
+        for k in range(len(left_sum)):
+            if left_sum[k]==right_sum[k]:
+                return k
+        return -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '''
         for i in range(len(nums)):
             if i==len(nums)-1:
                 suffix=0
@@ -18,4 +58,5 @@ class Solution(object):
             if prefix==suffix:
                 return i
         return -1
+        '''
         
