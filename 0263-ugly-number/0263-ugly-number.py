c80f244cc8be9761289ef15c421eab826f39cfdd
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-27 00:33:40 -0400

    Time: 2 ms (21.92%), Space: 12.4 MB (51.66%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,27 @@
+from math import sqrt
+class Solution(object):
+    def isUgly(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        curr=n
+        
+        if curr==0 or curr<0:
+            return False
+        
+        while curr!=1:
+            if curr%2==0:
+                curr=curr//2
+            elif curr%3==0:
+                curr=curr//3
+            elif curr%5==0:
+                curr=curr//5
+            else:
+                return False
+        
+        return True
+
+
+
+        
