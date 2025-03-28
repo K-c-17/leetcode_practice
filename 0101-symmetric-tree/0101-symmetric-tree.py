25dcd20624b1c20d3b10b78be4f90b59e132bb44
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-27 20:51:28 -0400

    Time: 4 ms (7.04%), Space: 12.7 MB (21.75%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -10,7 +10,42 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: bool
         """
+        #Iterative Implementation
+        if not root:
+            return True
+        
+        queue=collections.deque()
+        queue.append(root)
 
+        while queue:
+            temp=[]
+            for i in range(len(queue)):
+                curr=queue.popleft()
+                if curr:
+                    temp.append(curr.val)
+                    queue.append(curr.left)
+                    queue.append(curr.right)
+                else:
+                    temp.append(None)
+            
+            #print(temp)
+            if temp!=temp[::-1]:
+                return False
+        
+        return True
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #Recursive Implementation
+        '''
         def is_mirror(node1,node2):
             if node1 is None and node2 is None:
                 return True
@@ -22,6 +57,7 @@ class Solution(object):
             is_mirror(node1.right,node2.left))
 
         return is_mirror(root,root)
+        '''
 
         
         
