c953810e5872906b78da1115e913cb7a334be416
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-29 19:06:31 -0400

    Time: 11 ms (6.26%), Space: 12.5 MB (11.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -9,10 +9,18 @@ class Solution(object):
         for i in nums:
             count[i]+=1
         
+        start,end=0,0
+        for j in [0,1,2]:
+            end+=count[j]
+            nums[start:end]=[j]*count[j]
+            start=end
+            print(nums)
+        
+        '''
         nums[0:count[0]]=[0]*count[0]
         nums[count[0]:count[0]+count[1]]=[1]*count[1]
         nums[count[0]+count[1]:count[0]+count[1]+count[2]]=[2]*count[2]
-
+        '''
 
 
 
