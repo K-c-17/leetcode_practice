e7fa801a984a26fb0f5a5ca125ff3f00aabf2046
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-03-30 23:42:50 -0400

    Time: 80 ms (13.11%), Space: 38.2 MB (14.31%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        #connect=defaultdict(int)
-        connect=[0]*(len(edges)+2)
+        connect=defaultdict(int)
+        #connect=[0]*(len(edges)+2)
 
         for i in edges:
             connect[i[0]]+=1
             connect[i[1]]+=1
         
-        for j in range(1,len(connect)+2):
+        #for j in range(1,len(connect)+2):
+        for j in connect:
             if connect[j]>1:
                 return j
+        return -1
         
