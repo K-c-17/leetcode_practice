c53ebd65c2d3a2e1be49b7ab43aed538fa91457c
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-04-02 03:20:22 -0400

    Time: 0 ms (100%), Space: 12.3 MB (82.35%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        #divide and conquer without memoization
+        '''
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+        else:
+            subP1=self.climbStairs(n-1)
+            subP2=self.climbStairs(n-2)
+            return subP1+subP2
+        '''
+
+        dp={}
+        return self.climbStairs_helper(n,dp)
+    
+    def climbStairs_helper(self,n,dp):
+        if n==1:
+            return 1
+        elif n==2:
+            return 2
+        else:
+            if n not in dp:
+                subP1=self.climbStairs_helper(n-1,dp)
+                subP2=self.climbStairs_helper(n-2,dp)
+                dp[n]=subP1+subP2
+            return dp[n]
+
+        
