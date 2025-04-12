ce56d46bbbe410822b057def425b6339fafdcf4c
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-04-12 05:22:20 -0400

    Time: 1009 ms (5.07%), Space: 12.5 MB (77.09%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def findTargetSumWays(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if (sum(nums)-target)%2 != 0 or sum(nums)<target:
+            return 0
+        
+        s1=(sum(nums)-target)//2
+        print("s1: ",s1)
+
+        return self.subsetSum(nums,s1)
+
+    def subsetSum(self,arr,cap):
+        n=len(arr)
+        
+        #intialization of matrix
+        dp=[[0]*(cap+1) for _ in range(n+1)]
+        print(dp)
+        for i in range(n+1):
+            dp[i][0]=1
+        
+        for j in range(1,n+1):
+            for k in range(cap+1):
+                if arr[j-1]<=k:
+                    dp[j][k]=dp[j-1][k-arr[j-1]] + dp[j-1][k]
+                else:
+                    dp[j][k]=dp[j-1][k]
+        
+        return dp[n][cap]
+
+
+        
