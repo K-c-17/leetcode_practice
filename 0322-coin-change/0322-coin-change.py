b4685a8652f669e5a3e08373332b9cabdf21f652
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-04-14 02:18:41 -0400

    Time: 1203 ms (23.17%), Space: 17.1 MB (15.9%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -11,13 +11,13 @@ class Solution(object):
         dp=[[0]*(t+1) for _ in range(n+1)]
 
         for k in range(t+1):
-            dp[0][k]=float("inf")-1
+            dp[0][k]=float("inf")
         
         for l in range(1,t+1):
             if l%coins[0]==0:
                 dp[1][l]=l//coins[0]
             else:
-                dp[1][l]=float("inf")-1
+                dp[1][l]=float("inf")
 
         for i in range(2,n+1):
             for j in range(1,t+1):
