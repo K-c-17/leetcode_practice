f160949347ac9f8d178a8b2ee70b14bc65480344
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-04-16 22:52:06 -0400

    Time: 11 ms (60.03%), Space: 14 MB (99.82%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        freq=collections.Counter(nums)
+        
+        return sorted(freq,key=freq.get,reverse=True)[:k]
+
+        
