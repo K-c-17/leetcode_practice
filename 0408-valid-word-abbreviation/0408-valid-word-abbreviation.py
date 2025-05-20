0cb5b1bcba9925dbe3c8f36d2c2e43595aa8ffd7
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-05-20 18:29:40 -0400

    Time: 0 ms (100%), Space: 12.6 MB (12.95%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        i,j=0,0
+
+        while i<len(word) and j<len(abbr):
+            #if an alphabet
+            if abbr[j].isalpha():
+                if word[i]!=abbr[j]:
+                    print("first")
+                    return False
+                else:
+                    i+=1
+                    j+=1
+            #not an alphabet
+            elif abbr[j]=='0':
+                print("second")
+                return False
+            
+            elif abbr[j].isdigit():
+                num=''
+                while j<len(abbr) and abbr[j].isdigit():
+                    num+=abbr[j]
+                    j+=1
+                i+=int(num)
+        print("third")    
+        return (i==len(word)) and (j==len(abbr))
+            
