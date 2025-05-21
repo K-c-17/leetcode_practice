85ae296feb18cb413cca649eec217588f56eea47
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-05-21 17:47:44 -0400

    Time: 0 ms (100%), Space: 12.5 MB (52.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,27 +7,58 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        temp=[0 for i in range(m+n)]
+        last=m+n-1
 
-        i,j,k=0,0,0
 
-        while i<m and j<n:
-            if nums1[i]>nums2[j]:
-                temp[k]=nums2[j]
-                j+=1
-                k+=1
+        while m>0 and n>0:
+            if nums2[n-1]>nums1[m-1]:
+                nums1[last]=nums2[n-1]
+                n-=1
             else:
-                temp[k]=nums1[i]
-                i+=1
-                k+=1
+                nums1[last]=nums1[m-1]
+                m-=1
+            
+            last-=1
         
-        #print(temp,i,j,k)
-        if i<m:
-            temp[k:]=nums1[i:m]
-        elif j<n:
-            temp[k:]=nums2[j:]
+        while n>0:
+            nums1[last]=nums2[n-1]
+            n-=1
+            last-=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # temp=[0 for i in range(m+n)]
+
+        # i,j,k=0,0,0
+
+        # while i<m and j<n:
+        #     if nums1[i]>nums2[j]:
+        #         temp[k]=nums2[j]
+        #         j+=1
+        #         k+=1
+        #     else:
+        #         temp[k]=nums1[i]
+        #         i+=1
+        #         k+=1
         
-        nums1[::1]=temp[::1]
+        # #print(temp,i,j,k)
+        # if i<m:
+        #     temp[k:]=nums1[i:m]
+        # elif j<n:
+        #     temp[k:]=nums2[j:]
+        
+        # nums1[::1]=temp[::1]
 
 
         
