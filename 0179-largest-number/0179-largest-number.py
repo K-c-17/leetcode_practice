7bf2acfd408d9294af8e0ec58b051a38d57e48d1
kshitij <57230059+K-c-17@users.noreply.github.com>
2025-05-31 04:19:22 -0400

    Time: 0 ms (100%), Space: 12.4 MB (71.49%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -4,16 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: str
         """
+        #O(n) implementation
         nums=[str(x) for x in nums]
 
-        for i in range(len(nums)-1):
-            for j in range(len(nums)-i-1):
-                if nums[j]+nums[j+1]<nums[j+1]+nums[j]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-        
+        nums.sort(key=lambda y: 10*y, reverse=True)
+
         res=''.join(nums)
         
-        return '0' if res[0]=='0' else res
+        return '0' if res[0]=='0' else res      
+        
+        
+        
+        # #O(n**2) implementation (using bubble sort)
+        # nums=[str(x) for x in nums]
+
+        # for i in range(len(nums)-1):
+        #     for j in range(len(nums)-i-1):
+        #         if nums[j]+nums[j+1]<nums[j+1]+nums[j]:
+        #             nums[j],nums[j+1]=nums[j+1],nums[j]
+        
+        # res=''.join(nums)
+        
+        # return '0' if res[0]=='0' else res
 
 
 
